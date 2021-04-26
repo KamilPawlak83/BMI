70f92f8fd381dfe54068e5b1a6309cad9d265ac3
Kamil <kamilpawlak@op.pl>
2021-04-26 16:05:30 +0200

    Initial Commit

---
+++ BMI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI
+//
+//  Created by Kamil Pawlak on 26/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
