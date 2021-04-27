d28aec6a8b519f551ede3fc3d813fc32e20649dd
Kamil <kamilpawlak@op.pl>
2021-04-27 08:48:57 +0200

    zmieniłem linie na 0 bo 1 to był max

--- BMI/Controller/CalculateViewController.swift
+++ BMI/Controller/CalculateViewController.swift
@@ -20,24 +20,17 @@ class CalculateViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
     @IBAction func heightSliderChanged(_ sender: UISlider) {
         let height = String(format: "%.2f", sender.value)
         heightLabel.text = "\(height)m"
-        
     }
  
     
     @IBAction func weightSliderChanged(_ sender: UISlider) {
-        
-        
         let weight = String(format: "%.0f", sender.value)
         weightLabel.text = "\(weight)Kg"
-       // ewentualnie weightLabel.text = String(Int(sender.value))
-        
-        
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
